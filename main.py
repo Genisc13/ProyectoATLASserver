01416a6b621f7a3a7cdac3621b72cd82d0ef8544
genis_castillo <genis.castillo@estudiantat.upc.edu>
2024-05-11 18:58:13 +0200

    creada clase database

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-#import database as db
+# import database as db
 from pydantic import BaseModel
 import uvicorn
-import prueba_database as sql
+from prueba_database import Database as Sql
 
 '''TERMINAL
 Para crear un entorno virutal (Mac) -> python3 -m venv nombre_del_entorno_virtual
@@ -13,6 +13,7 @@ para parar el servidor control+c
 para salir del entorno -> deactivate
 '''
 app = FastAPI()
+
 
 class Drone(BaseModel):
     name: str
@@ -28,23 +29,27 @@ class Drone(BaseModel):
 def root():
     return 'esto va?'
 
+
 @app.post('/add_drones')
-def entrar_dron(drone:Drone):
+def entrar_dron(drone: Drone):
     try:
-        x = sql.insertar_dron(drone.name,drone.chasis,drone.brazos,drone.helices,drone.bateria,drone.sensores,drone.camara)  
+        x = sql.insertar_dron(drone.name, drone.chasis, drone.brazos, drone.helices, drone.bateria, drone.sensores,
+                              drone.camara)
         if x == 0:
-            return('se ha insertado con exito') 
+            return ('se ha insertado con exito')
         else:
             return x
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
-    
+
+
 @app.get("/muestra_drones")
 def muestra_dron():
     return sql.mostrar_tabla()
 
+
 @app.delete('/eliminar_dron/{name}')
-def delete_dron(name:str):
+def delete_dron(name: str):
     try:
         y = sql.elmiminar_dron(name)
         if y == 0:
@@ -53,23 +58,24 @@ def delete_dron(name:str):
             return y
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
-    
+
 
 @app.put("/update_name/{name},{name_viejo}")
-def update_dron(name:str,name_viejo:str):
+def update_dron(name: str, name_viejo: str):
     try:
-        z = sql.modificar_nombre(name,name_viejo)
+        z = sql.modificar_nombre(name, name_viejo)
         if z == 0:
             return 'Se ha cambiado con exito el nombre'
         else:
             return z
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
-    
+
+
 @app.put("/update_chasis/{name},{chasis}")
-def update_chasis(name:str,chasis:str):
+def update_chasis(name: str, chasis: str):
     try:
-        z = sql.modificar_chasis(chasis,name)
+        z = sql.modificar_chasis(chasis, name)
         if z == 0:
             return 'Se ha cambiado con exito el chasis'
         else:
@@ -77,10 +83,11 @@ def update_chasis(name:str,chasis:str):
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
 
+
 @app.put("/update_brazos/{name},{brazos}")
-def update_brazos(name:str,brazos:int):
+def update_brazos(name: str, brazos: int):
     try:
-        z = sql.modificar_brazos(brazos,name)
+        z = sql.modificar_brazos(brazos, name)
         if z == 0:
             return 'Se ha cambiado con exito el numero de brazos'
         else:
@@ -88,10 +95,11 @@ def update_brazos(name:str,brazos:int):
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
 
+
 @app.put("/update_helices/{name},{helices}")
-def update_helices(name:str,helices:int):
+def update_helices(name: str, helices: int):
     try:
-        z = sql.modificar_helices(helices,name)
+        z = sql.modificar_helices(helices, name)
         if z == 0:
             return 'Se ha cambiado con exito el numero de helices'
         else:
@@ -99,34 +107,35 @@ def update_helices(name:str,helices:int):
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
 
+
 @app.put("/update_bateria/{name},{bateria}")
-def update_bateria(name:str,bateria:float):
+def update_bateria(name: str, bateria: float):
     try:
-        z = sql.modificar_bateria(bateria,name)
+        z = sql.modificar_bateria(bateria, name)
         if z == 0:
             return 'Se ha cambiado con exito la bateria'
         else:
             return z
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
-    
 
 
 @app.put("/update_sensores/{name},{sensores}")
-def update_sensores(name:str,sensores:int):
+def update_sensores(name: str, sensores: int):
     try:
-        z = sql.modificar_sensores(sensores,name)
+        z = sql.modificar_sensores(sensores, name)
         if z == 0:
             return 'Se ha cambiado con exito el numero de sensores'
         else:
             return z
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
-    
+
+
 @app.put("/update_camara/{name},{camara}")
-def update_cam(name:str,camara:str):
+def update_cam(name: str, camara: str):
     try:
-        z = sql.modificar_camara(camara,name)
+        z = sql.modificar_camara(camara, name)
         if z == 0:
             return 'Se ha cambiado con exito la camara'
         else:
@@ -134,7 +143,9 @@ def update_cam(name:str,camara:str):
     except Exception as e:
         return f"Algo no ha ido bien: {str(e)}"
 
-#Esto es para que una vez se incia con el reload se vaya actualizando y no haya que meter en terminal el reload 
-#cada vez que hace un cambio.
+
+# Esto es para que una vez se incia con el reload se vaya actualizando y no haya que meter en terminal el reload
+# cada vez que hace un cambio.
 if __name__ == "__main__":
+    sql = Sql("localhost", "root", "password", "ATLAS_DB")
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- prueba_database.py
+++ prueba_database.py
@@ -1,5 +1,5 @@
-#sudo /usr/local/mysql/support-files/mysql.server start (Activar server desde terminal MAC)
-#mysql -u root -p (Activar mySQL)
+# sudo /usr/local/mysql/support-files/mysql.server start (Activar server desde terminal MAC)
+# mysql -u root -p (Activar mySQL)
 import pymysql
 
 """
@@ -15,137 +15,157 @@ INSERT
 exit (para salir)
 """
 
-conn = pymysql.connect(host='localhost', user='root',passwd='password',database='DRONES')
-c = conn.cursor()
 
-c.execute(''' CREATE TABLE IF NOT EXISTS drones (
-          name TEXT,
-          chasis TEXT,
-          brazos INTEGER,
-          helices INTEGER,
-          bateria REAL,
-          Sensores INTEGER,
-          camara TEXT
-          )''')
-conn.commit()
-conn.close()
+class Database:
+    def __init__(self, host, user, password, name):
+        super().__init__()
+        self.database_host = host
+        self.database_user = user
+        self.database_password = password
+        self.database_name = name
+        self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password)
+        c = self.conn.cursor()
+        c.execute(f"CREATE DATABASE ${self.database_name}")
+        self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                    database=self.database_name)
+        c = self.conn.cursor()
+        c.execute(''' CREATE TABLE IF NOT EXISTS drones (
+                  name TEXT,
+                  chasis TEXT,
+                  brazos INTEGER,
+                  helices INTEGER,
+                  bateria REAL,
+                  Sensores INTEGER,
+                  camara TEXT
+                  )''')
+        self.conn.commit()
+        self.conn.close()
 
-def insertar_dron(name:str,chasis:str,brazos:int,helices:int,bateria:float,sensores:int,camara:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor()   
-        c.execute("INSERT INTO drones VALUES (%s, %s, %s, %s, %s, %s, %s)", (name, chasis, brazos, helices, bateria, sensores, camara))
-        conn.commit()
-        conn.close()
-        return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
-
-def mostrar_tabla():
-    conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-    c = conn.cursor() 
-    c.execute("SELECT * FROM drones")
-    data = c.fetchall()
-    conn.commit()
-    conn.close()
-    print(data)
-    return {'data':data}
-
-
-def elmiminar_dron(name:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor() 
-        c.execute("DELETE FROM drones WHERE name = %s",(name,))
-        if c.rowcount == 0:
-            conn.commit()
-            conn.close()
-            return ("No hay ningún dron guardado con ese nombre.")
-        else:
-            conn.commit()
-            conn.close()
+    def insertar_dron(self, name: str, chasis: str, brazos: int, helices: int, bateria: float, sensores: int,
+                      camara: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("INSERT INTO drones VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                      (name, chasis, brazos, helices, bateria, sensores, camara))
+            self.conn.commit()
+            self.conn.close()
             return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
 
+    def mostrar_tabla(self):
+        self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                    database=self.database_name)
+        c = self.conn.cursor()
+        c.execute("SELECT * FROM drones")
+        data = c.fetchall()
+        self.conn.commit()
+        self.conn.close()
+        print(data)
+        return {'data': data}
 
-def modificar_nombre(name:str,nom_viejo:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor() 
-        c.execute("UPDATE drones SET name=%s WHERE name=%s",(name,nom_viejo,))
-        conn.commit()
-        conn.close()
-        return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
-    
-def modificar_chasis(chasis:str, nombre:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor() 
-        c.execute("UPDATE drones SET chasis=%s WHERE name=%s",(chasis,nombre,))
-        conn.commit()
-        conn.close()
-        return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
+    def elmiminar_dron(self, name: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("DELETE FROM drones WHERE name = %s", (name,))
+            if c.rowcount == 0:
+                self.conn.commit()
+                self.conn.close()
+                return ("No hay ningún dron guardado con ese nombre.")
+            else:
+                self.conn.commit()
+                self.conn.close()
+                return 0
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
 
-def modificar_brazos(brazos:int,name:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor() 
-        c.execute("UPDATE drones SET brazos=%s WHERE name=%s",(brazos,name,))
-        conn.commit()
-        conn.close()
-        return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
-    
-def modificar_helices(helices:int,name:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor() 
-        c.execute("UPDATE drones SET helices=%s WHERE name=%s",(helices,name,))
-        conn.commit()
-        conn.close()
-        return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
-    
-def modificar_bateria(bateria:float,name:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor() 
-        c.execute("UPDATE drones SET bateria=%s WHERE name=%s",(bateria,name,))
-        conn.commit()
-        conn.close()
-        return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
-    
-def modificar_sensores(sensores:int,name:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor() 
-        c.execute("UPDATE drones SET Sensores=%s WHERE name=%s",(sensores,name,))
-        conn.commit()
-        conn.close()
-        return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
-    
-def modificar_camara(cam:str,name:str):
-    try:
-        conn = pymysql.connect(host='localhost', user='root',passwd='password',db='DRONES')
-        c = conn.cursor() 
-        c.execute("UPDATE drones SET camara=%s WHERE name=%s",(cam,name))
-        conn.commit()
-        conn.close()
-        return 0
-    except Exception as e:
-        return f'Ha habido algun error en la base de datos: {str(e)}'
-    
+    def modificar_nombre(self, name: str, nom_viejo: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("UPDATE drones SET name=%s WHERE name=%s", (name, nom_viejo,))
+            self.conn.commit()
+            self.conn.close()
+            return 0
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
 
-#insertar_dron('Prueba','carbono',4,4,67.6,8,'4k')   
-#mostrar_tabla()
+    def modificar_chasis(self, chasis: str, nombre: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("UPDATE drones SET chasis=%s WHERE name=%s", (chasis, nombre,))
+            self.conn.commit()
+            self.conn.close()
+            return 0
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
+
+    def modificar_brazos(self, brazos: int, name: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("UPDATE drones SET brazos=%s WHERE name=%s", (brazos, name,))
+            self.conn.commit()
+            self.conn.close()
+            return 0
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
+
+    def modificar_helices(self, helices: int, name: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("UPDATE drones SET helices=%s WHERE name=%s", (helices, name,))
+            self.conn.commit()
+            self.conn.close()
+            return 0
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
+
+    def modificar_bateria(self, bateria: float, name: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("UPDATE drones SET bateria=%s WHERE name=%s", (bateria, name,))
+            self.conn.commit()
+            self.conn.close()
+            return 0
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
+
+    def modificar_sensores(self, sensores: int, name: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("UPDATE drones SET Sensores=%s WHERE name=%s", (sensores, name,))
+            self.conn.commit()
+            self.conn.close()
+            return 0
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
+
+    def modificar_camara(self, cam: str, name: str):
+        try:
+            self.conn = pymysql.connect(host=self.database_host, user=self.database_user, passwd=self.database_password,
+                                        database=self.database_name)
+            c = self.conn.cursor()
+            c.execute("UPDATE drones SET camara=%s WHERE name=%s", (cam, name))
+            self.conn.commit()
+            self.conn.close()
+            return 0
+        except Exception as e:
+            return f'Ha habido algun error en la base de datos: {str(e)}'
+
+    # insertar_dron('Prueba','carbono',4,4,67.6,8,'4k')
+    # mostrar_tabla()
